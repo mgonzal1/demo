1e63fcc61537a1bc446fdc801ce1a26bb1886186
Mariana González Velarde <mariana@fnal.gov>
2025-08-15 11:38:35 -0500

    Fix mymath module name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="math",
+    name="mymath",
     version="0.1.0",
     packages=find_packages(where="mymath"),
     package_dir={"": "mymath"},
